ecf1dfa57244820364176017e05c814b6efdb33b
GrafPlay <90318901+GrafPlay@users.noreply.github.com>
2021-12-04 08:22:34 +0300

    Add files via upload

---
+++ Отчет 04.12.21/04.12.21/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите значение а");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение b");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение c");
+int c = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение d");
+int d = int.Parse(Console.ReadLine());
+int x = 0;
+int y = 0;
+if (a < b)
+{
+    x = a;
+}
+else
+{
+    x = b;
+}
+if (c < d)
+{
+    y = c;
+}
+else
+{
+    y = d;
+}
+if (x < y)
+{
+    Console.WriteLine(x);
+}
+else
+{
+    Console.WriteLine(y);
+}
